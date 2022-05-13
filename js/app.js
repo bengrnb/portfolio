b335686b02e728ab9bcfbc8b87b3ac96e3a48423
bengrnb <66656217+bengrnb@users.noreply.github.com>
2022-05-13 11:54:18 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+let about = document.querySelector(".home-about")
+let aboutImg = document.querySelector("#about-image");
+about.addEventListener('mouseover', function(){
+    aboutImg.src = "./images/ellipse_hover.svg"
+})
+about.addEventListener('mouseout', function(){
+    aboutImg.src = "./images/ellipse.svg"
+})
+
+let portfolio = document.querySelector(".home-portfolio")
+let portfolioImg = document.querySelector("#portfolio-image");
+portfolio.addEventListener('mouseover', function(){
+    portfolioImg.src = "./images/triangle_hover.svg"
+})
+portfolio.addEventListener('mouseout', function(){
+    portfolioImg.src = "./images/triangle.svg"
+})
+
+let contact = document.querySelector(".home-contact")
+let contactImg = document.querySelector("#contact-image");
+contact.addEventListener('mouseover', function(){
+    contactImg.src = "./images/hexagon_hover.svg"
+})
+contact.addEventListener('mouseout', function(){
+    contactImg.src = "./images/hexagon.svg"
+})
