a493d7f745af2b6c72650ff728d15ec7602aca27
bengrnb <66656217+bengrnb@users.noreply.github.com>
2022-05-13 13:48:26 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ contact.addEventListener('touchend', function(){
     contact.style.backgroundColor='#955BA5';
     contact.style.color='White';
 })
-contact.addEventListener('popstate', function(){
+window.addEventListener('popstate', function(){
     contactImg.src = "./images/hexagon.svg";
     contact.style.backgroundColor='#955BA5';
     contact.style.color='White';
