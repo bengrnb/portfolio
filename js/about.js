746f915c4b9790fc4dd0e72db77bf7a4e9331f59
bengrnb <66656217+bengrnb@users.noreply.github.com>
2022-05-16 22:50:09 -0400

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -1,21 +1,27 @@
-let download = document.querySelector("#download");
+let download = document.querySelector(".download");
+let resumeImg = document.querySelector("#resume-image");
 download.addEventListener('mouseover', function(){
+    resumeImg.src = "./images/resume_hover.svg";
     download.style.backgroundColor='White';
     download.style.color='Black';
 })
 download.addEventListener('mouseout', function(){
-    download.style.backgroundColor='#4EBA6F';
+    resumeImg.src = "./images/resume.svg";
+    download.style.backgroundColor='#2D95BF';
     download.style.color='White';
 })
 download.addEventListener('touchstart', function(){
+    resumeImg.src = "./images/resume_hover.svg";
     download.style.backgroundColor='White';
     download.style.color='Black';
 })
 download.addEventListener('touchend', function(){
-    download.style.backgroundColor='#4EBA6F';
+    resumeImg.src = "./images/resume.svg";
+    download.style.backgroundColor='#2D95BF';
     download.style.color='White';
 })
 download.addEventListener('click', function(){
-    download.style.backgroundColor='#4EBA6F';
+    resumeImg.src = "./images/resume.svg";
+    download.style.backgroundColor='#2D95BF';
     download.style.color='White';
 })
