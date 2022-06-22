1f6b325b850577106dd42720f04f8851f47ab446
bengrnb <66656217+bengrnb@users.noreply.github.com>
2022-06-22 00:10:22 -0400

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,70 @@
+let CAPS = document.querySelector("#CAPS");
+CAPS.addEventListener('mouseover', function(){
+    CAPS.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+})
+CAPS.addEventListener('mouseout', function(){
+    CAPS.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+CAPS.addEventListener('touchstart', function(){
+    CAPS.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+})
+CAPS.addEventListener('touchend', function(){
+    CAPS.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+CAPS.addEventListener('click', function(){
+    CAPS.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+
+
+let SPACES = document.querySelector("#SPACES");
+SPACES.addEventListener('mouseover', function(){
+    SPACES.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+})
+SPACES.addEventListener('mouseout', function(){
+    SPACES.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+SPACES.addEventListener('touchstart', function(){
+    SPACES.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+})
+SPACES.addEventListener('touchend', function(){
+    SPACES.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+SPACES.addEventListener('click', function(){
+    SPACES.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+
+
+let CDL = document.querySelector("#CDL");
+CDL.addEventListener('mouseover', function(){
+    CDL.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+})
+CDL.addEventListener('mouseout', function(){
+    CDL.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+CDL.addEventListener('touchstart', function(){
+    CDL.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+})
+CDL.addEventListener('touchend', function(){
+    CDL.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+CDL.addEventListener('click', function(){
+    CDL.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+
+
+let CAH = document.querySelector("#CAH");
+CAH.addEventListener('mouseover', function(){
+    CAH.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+})
+CAH.addEventListener('mouseout', function(){
+    CAH.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+CAH.addEventListener('touchstart', function(){
+    CAH.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+})
+CAH.addEventListener('touchend', function(){
+    CAH.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
+CAH.addEventListener('click', function(){
+    CAH.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+})
