590e73d0607e8a905726e433842d0f223f1ae93c
bengrnb <66656217+bengrnb@users.noreply.github.com>
2022-06-24 19:23:11 -0400

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,70 +1,90 @@
 let CAPS = document.querySelector("#CAPS");
 CAPS.addEventListener('mouseover', function(){
     CAPS.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+    CAPS.style.backgroundSize= '520px';
 })
 CAPS.addEventListener('mouseout', function(){
     CAPS.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    CAPS.style.backgroundSize= '500px';
 })
 CAPS.addEventListener('touchstart', function(){
     CAPS.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+    CAPS.style.backgroundSize= '520px';
 })
 CAPS.addEventListener('touchend', function(){
     CAPS.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    CAPS.style.backgroundSize= '500px';
 })
 CAPS.addEventListener('click', function(){
     CAPS.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    CAPS.style.backgroundSize= '500px';
 })
 
 
 let SPACES = document.querySelector("#SPACES");
 SPACES.addEventListener('mouseover', function(){
     SPACES.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+    SPACES.style.backgroundSize= '520px';
 })
 SPACES.addEventListener('mouseout', function(){
     SPACES.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    SPACES.style.backgroundSize= '500px';
 })
 SPACES.addEventListener('touchstart', function(){
     SPACES.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+    SPACES.style.backgroundSize= '520px';
 })
 SPACES.addEventListener('touchend', function(){
     SPACES.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    SPACES.style.backgroundSize= '500px';
 })
 SPACES.addEventListener('click', function(){
     SPACES.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    SPACES.style.backgroundSize= '500px';
 })
 
 
 let CDL = document.querySelector("#CDL");
 CDL.addEventListener('mouseover', function(){
     CDL.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+    CDL.style.backgroundSize= '520px';
 })
 CDL.addEventListener('mouseout', function(){
     CDL.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    CDL.style.backgroundSize= '500px';
 })
 CDL.addEventListener('touchstart', function(){
     CDL.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+    CDL.style.backgroundSize= '520px';
 })
 CDL.addEventListener('touchend', function(){
     CDL.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    CDL.style.backgroundSize= '500px';
 })
 CDL.addEventListener('click', function(){
     CDL.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    CDL.style.backgroundSize= '500px';
 })
 
 
 let CAH = document.querySelector("#CAH");
 CAH.addEventListener('mouseover', function(){
     CAH.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+    CAH.style.backgroundSize= '520px';
 })
 CAH.addEventListener('mouseout', function(){
     CAH.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    CAH.style.backgroundSize= '500px';
 })
 CAH.addEventListener('touchstart', function(){
     CAH.style.boxShadow='0px 0px 15px 0px rgba(174, 174, 174, 1)';
+    CAH.style.backgroundSize= '520px';
 })
 CAH.addEventListener('touchend', function(){
     CAH.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    CAH.style.backgroundSize= '500px';
 })
 CAH.addEventListener('click', function(){
     CAH.style.boxShadow='0px 0px 8px 0px rgba(227, 227, 227, 1)';
+    CAH.style.backgroundSize= '500px';
 })
