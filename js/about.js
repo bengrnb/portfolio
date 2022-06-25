5166787792f0ec05d1b89c4c0b053cfe5d7f8bcb
bengrnb <66656217+bengrnb@users.noreply.github.com>
2022-06-25 13:13:38 -0400

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -1,27 +1,27 @@
 let download = document.querySelector(".download");
 let resumeImg = document.querySelector("#resume-image");
 download.addEventListener('mouseover', function(){
-    resumeImg.src = "./images/about_images/resume_hover.svg";
-    download.style.backgroundColor='White';
-    download.style.color='Black';
+    resumeImg.src = "./images/about_images/resume.svg";
+    download.style.backgroundColor='#2D95BF';
+    download.style.color='White';
 })
 download.addEventListener('mouseout', function(){
-    resumeImg.src = "./images/about_images/resume.svg";
-    download.style.backgroundColor='#2D95BF';
-    download.style.color='White';
-})
-download.addEventListener('touchstart', function(){
     resumeImg.src = "./images/about_images/resume_hover.svg";
     download.style.backgroundColor='White';
     download.style.color='Black';
 })
-download.addEventListener('touchend', function(){
+download.addEventListener('touchstart', function(){
     resumeImg.src = "./images/about_images/resume.svg";
     download.style.backgroundColor='#2D95BF';
     download.style.color='White';
 })
+download.addEventListener('touchend', function(){
+    resumeImg.src = "./images/about_images/resume_hover.svg";
+    download.style.backgroundColor='White';
+    download.style.color='Black';
+})
 download.addEventListener('click', function(){
-    resumeImg.src = "./images/about_images/resume.svg";
-    download.style.backgroundColor='#2D95BF';
-    download.style.color='White';
+    resumeImg.src = "./images/about_images/resume_hover.svg";
+    download.style.backgroundColor='White';
+    download.style.color='Black';
 })
